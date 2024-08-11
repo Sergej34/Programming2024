87220b84a42a4166a3c6acccf70831a544cd6864
Sergej34 <bmw325d@web.de>
2024-08-11 22:15:05 +0200

    detail project

--- src/ait/detail/test/StockImplTest.java
+++ src/ait/detail/test/StockImplTest.java
@@ -19,7 +19,7 @@ class StockImplTest {
     void setUp() {
 
         stock = new StockImpl(5);
-        details = new Detail[6];
+        details = new Detail[4];
         details[0] = new Gear(1L, 10, "steal", "Bosh", 2.0, 12, 6);
         details[1] = new Gear(2L, 10, "steal", "Bosh", 2.0, 24, 8);
         details[2] = new Lever(3L, 100, "steal", "Bosh", 3.0, 12);
@@ -27,6 +27,7 @@ class StockImplTest {
 
         for (int i = 0; i < details.length; i++) {
             stock.addDetail(details[i]);
+            //System.out.println(details[i]);
         }
     }
 
@@ -34,7 +35,7 @@ class StockImplTest {
     void addDetail() {
         assertFalse(stock.addDetail(details[1]));// добавление дубликата
         assertFalse(stock.addDetail(null));// добавление null
-        Detail detail = new Gear(5L, 10, "steal", "Bosh", 2.0, 24, 12);
+        Detail detail = new Gear(5L, 10, "Stahl", "Bosch", 2.0, 24, 12);
         assertTrue(stock.addDetail(detail));// добавление новой детали
         assertEquals(5, stock.quantity());// проверка количества деталей на складе
         // Теперь склад полон, добавление новой детали должно вернуть false
@@ -55,9 +56,8 @@ class StockImplTest {
     void updateDetail() {
         Detail detail = new Gear(2L, 10, "steal", "Bosh2", 2.0, 24, 14);
         assertEquals(details[1], stock.updateDetail(detail));
-        detail = new Lever(5L, 100, "steal", "Bosh", 3.0, 12);
+        detail = new Lever(5L, 100, "steal", "Bosh", 3.0, 10);
         assertNull(stock.updateDetail(detail));
-        System.out.println(detail);
     }
 
     @Test
